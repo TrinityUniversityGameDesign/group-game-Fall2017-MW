791fe0de40b95bb3706d3019d684fc61a82dde89
tykirby11 <tyakirby@gmail.com>
2017-10-25 15:41:26 -0500

    Menu STill not working

--- Assets/Scripts/Menu.cs
+++ Assets/Scripts/Menu.cs
@@ -6,9 +6,9 @@ using UnityEngine.UI;
 
 public class Menu : MonoBehaviour {
 
-   // public string GameStart;
-   
+    // public string GameStart;
 
+    public string startgame;
 
 	// Use this for initialization
 
@@ -17,11 +17,13 @@ public class Menu : MonoBehaviour {
 
     }
 	public void StartGame () {
-        SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex + 1);
+        print("Start Pushed");
+        SceneManager.LoadScene(startgame);// (SceneManager.GetActiveScene().buildIndex + 1);
 	}
 	
 	// Update is called once per frame
 	public void Quit () {
+        print("Quit Pushed");
         Application.Quit();
 	}
 }
