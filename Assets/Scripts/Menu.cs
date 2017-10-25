703a50099a47cb4f064543a610251e160b21498f
tykirby11 <tyakirby@gmail.com>
2017-10-25 14:48:59 -0500

    Menu Script

---
+++ Assets/Scripts/Menu.cs
@@ -0,0 +1,37 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+using UnityEngine.UI;
+
+public class Menu : MonoBehaviour {
+
+    private Timer StartingTime;
+    private DropOff StartWinOff;
+
+
+    public Text highscoreText;
+    public string GameStart;
+   
+
+
+	// Use this for initialization
+
+     void Start()
+    {
+        highscoreText.text = "Most Recent Time: " + PlayerPrefs.GetString("Most Recent Time");
+    }
+	public void StartGame () {
+        SceneManager.LoadScene(GameStart);
+        StartingTime.StartTime = 0f;
+        StartWinOff.win = false;
+
+        StartingTime.StartTime = Time.time;
+        
+	}
+	
+	// Update is called once per frame
+	public void Quit () {
+        Application.Quit();
+	}
+}
