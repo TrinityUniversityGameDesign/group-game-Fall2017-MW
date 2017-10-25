4396b4b5875ab4a6a947f3a052a5e7f4c3a07476
cescobed <cescobed@trinity.edu>
2017-10-25 15:43:21 -0500

    addedJumpingNeedsFixing

--- Assets/Scripts/PlayerMovementForBossBattle.cs
+++ Assets/Scripts/PlayerMovementForBossBattle.cs
@@ -2,7 +2,7 @@
 using System.Collections.Generic;
 using UnityEngine;
 
-public class PlayerMovement : MonoBehaviour {
+public class PlayerMovementForBossBattle : MonoBehaviour {
 	public float speed = 5f; 
 	private Rigidbody2D theRigidBody; 
 
@@ -15,9 +15,12 @@ public class PlayerMovement : MonoBehaviour {
 	// Update is called once per frame
 	void Update () {
 		float inputX = Input.GetAxis ("Horizontal");
-
 		theRigidBody.velocity = new Vector2 (inputX * speed, theRigidBody.velocity.y);
 
-		
+		if (Input.GetKey (KeyCode.Space)) {
+			theRigidBody.AddForce(Vector2.up * 50);
+		}
+
+	
 	}
 }
