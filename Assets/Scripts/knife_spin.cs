278369680cb10438e36967fdf007f90bbf9103d4
Gerardo Gonzalez <32018140+ggonzal2@users.noreply.github.com>
2017-10-25 15:22:56 -0500

    Weapons 1
    Spining knife
    Mallet in progress

---
+++ Assets/Scripts/knife_spin.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class knife_spin : MonoBehaviour
+{
+    //above 500 works best 
+    public float z;
+    // Use this for initialization
+    void Start()
+    {
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        //rotates knife z times per sec
+        transform.Rotate(0, 0, z * Time.deltaTime);
+    }
+}
