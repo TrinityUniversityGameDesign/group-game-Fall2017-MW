fdde16010534fc201a324a69f319d50b0cca460c
Alexander Hicks <alexander@alexanders-mbp.trinity.local>
2017-10-26 23:47:16 -0500

    Added basic player movement

---
+++ Assets/Scripts/Conveyer/ConveyerPlayerMovement.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ConveyerPlayerMovement : MonoBehaviour {
+
+	public float speed = 4;
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		transform.position += new Vector3 ((GlobalControl.GetHorizontal (1)*0.05f*speed), (GlobalControl.GetVertical (1)*0.05f*speed), 0);
+	}
+}
