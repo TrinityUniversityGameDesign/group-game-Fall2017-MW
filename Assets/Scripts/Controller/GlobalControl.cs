40733b7395b90d5d3e44412c293165fdb1626d64
bwalker4 <31222239+bwalker4@users.noreply.github.com>
2017-10-29 14:37:22 -0500

    Finished control class / playerJoin script
    Controls should be entirely set up now, but testing is needed.

--- Assets/Scripts/Controller/GlobalControl.cs
+++ Assets/Scripts/Controller/GlobalControl.cs
@@ -6,11 +6,19 @@ public static class GlobalControl {
 
     #region public variables
 
-    public static int NumPlayers;
+    public static int NumPlayers = 0;
 
     #endregion
 
-    private static bool downRT=false; // Will need to be an array later
+    private static bool[] downRT= {true, false, false, false, false };
+    private static int[] players = {-1, 0, 0, 0, 0 };
+
+    public static void AddPlayer(int controllerNumber)
+    {
+        players[NumPlayers] = controllerNumber;
+        NumPlayers += 1;
+    }
+
 
     #region public methods
 
@@ -19,85 +27,85 @@ public static class GlobalControl {
     // For left joystick
     public static float GetHorizontal(int playerNum)
     {
-        return Input.GetAxis("Horizontal_P1");
+        return Input.GetAxis("Horizontal_P" + players[playerNum]);
     }
     public static float GetVertical(int playerNum)
     {
-        return Input.GetAxis("Vertical_P1");
+        return Input.GetAxis("Vertical_P" + players[playerNum]);
     }
 
     // For right joystick
     public static float GetHorizontalAim(int playerNum)
     {
-        return Input.GetAxis("Aim_Horizontal_P1");
+        return Input.GetAxis("Aim_Horizontal_P" + players[playerNum]);
     }
     public static float GetVerticalAim(int playerNum)
     {
-        return Input.GetAxis("Aim_Vertical_P1");
+        return Input.GetAxis("Aim_Vertical_P" + players[playerNum]);
     }
 
     // Get Button: every frame
     public static bool GetButtonA(int playerNum)
     {
-        return Input.GetButton("A_P1");
+        return Input.GetButton("A_P" + players[playerNum]);
     }
     public static bool GetButtonB(int playerNum)
     {
-        return Input.GetButton("B_P1");
+        return Input.GetButton("B_P" + players[playerNum]);
     }
     public static bool GetButtonX(int playerNum)
     {
-        return Input.GetButton("X_P1");
+        return Input.GetButton("X_P" + players[playerNum]);
     }
     public static bool GetButtonY(int playerNum)
     {
-        return Input.GetButton("Y_P1");
+        return Input.GetButton("Y_P" + players[playerNum]);
     }
     public static bool GetButtonRT(int playerNum)
     {
-        return (Input.GetAxis("RT_P1") > .5f);
+        return (Input.GetAxis("RT_P" + players[playerNum]) > .5f);
     }
 
     // Get Button Down: the first frame only
     public static bool GetButtonDownA(int playerNum)
     {
-        return Input.GetButtonDown("A_P1");
+        return Input.GetButtonDown("A_P" + players[playerNum]);
     }
     public static bool GetButtonDownB(int playerNum)
     {
-        return Input.GetButtonDown("B_P1");
+        return Input.GetButtonDown("B_P" + players[playerNum]);
     }
     public static bool GetButtonDownX(int playerNum)
     {
-        return Input.GetButtonDown("X_P1");
+        return Input.GetButtonDown("X_P" + players[playerNum]);
     }
     public static bool GetButtonDownY(int playerNum)
     {
-        return Input.GetButtonDown("Y_P1");
+        return Input.GetButtonDown("Y_P" + players[playerNum]);
     }
     public static bool GetButtonDownRT(int playerNum)
     {
         if(GetButtonRT(playerNum))
         {
-            if (!downRT)
+            if (!downRT[playerNum])
             {
-                downRT = true;
+                downRT[playerNum] = true;
                 return true;
             }
             else return false;
         } else
         {
-            downRT = false;
+            downRT[playerNum] = false;
             return false;
         }
     }
     public static bool GetButtonDownStart(int playerNum)
     {
-        return Input.GetButtonDown("Start_P1");
+        return Input.GetButtonDown("Start_P" + players[playerNum]);
     }
     public static bool GetButtonDownBack(int playerNum)
     {
-        return Input.GetButtonDown("Back_P1");
+        return Input.GetButtonDown("Back_P" + players[playerNum]);
     }
 
     #endregion
