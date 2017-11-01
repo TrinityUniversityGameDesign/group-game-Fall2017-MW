55606573b600a1f62f7d3964b3fa85ad70b83bc5
tykirby11 <tyakirby@gmail.com>
2017-11-01 15:04:55 -0500

    Player and boss health

---
+++ Assets/Scripts/BossBattleScripts/BossHealth.cs
@@ -0,0 +1,32 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BossHealth : MonoBehaviour
+{
+
+
+    public float Bosshealth = 20;
+
+    // Use this for initialization
+    void Start()
+    {
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+    }
+
+
+    void OnCollisionEnter2d(Collision2D other)
+    {
+        if (other.gameObject.name == "pizza")
+        {
+            Bosshealth -= 1;
+            print(Bosshealth);
+        }
+    }
+}
---
+++ Assets/Scripts/BossBattleScripts/PlayerHealth.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerHealth : MonoBehaviour {
+
+
+    public float Playerhealth = 5;
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+
+
+    void OnCollisionEnter2d(Collision2D other)
+    {
+        if(other.gameObject.name == "knife")
+        {
+            Playerhealth -= 1;
+            print(Playerhealth);
+        }
+    }
+}
