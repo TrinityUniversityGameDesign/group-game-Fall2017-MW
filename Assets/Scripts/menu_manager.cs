36255d7efbf529fd106832245cf48e18d1163b59
Gerardo Gonzalez <32018140+ggonzal2@users.noreply.github.com>
2017-11-06 21:59:11 -0600

    7
    7

--- Assets/Scripts/menu_manager.cs
+++ Assets/Scripts/menu_manager.cs
@@ -1,6 +1,7 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.SceneManagement;
 
 public class menu_manager : MonoBehaviour {
 
@@ -11,6 +12,9 @@ public class menu_manager : MonoBehaviour {
 	
 	// Update is called once per frame
 	void Update () {
-		
+        if (GlobalControl.GetButtonA(1))
+        {
+
+        }	
 	}
 }
