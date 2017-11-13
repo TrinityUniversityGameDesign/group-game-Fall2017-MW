5defd29690ee28ea66b5d2b5c4da3ae6d5532b28
mhibbs <mhibbs@trinity.edu>
2017-11-13 15:28:18 -0600

    Merge branch 'master' into YummyMummy

--- Assets/Scripts/PlayerState.cs
+++ Assets/Scripts/PlayerState.cs
@@ -8,6 +8,7 @@ public static class PlayerState {
 
 	public static PlayerType[] playerType;
 
+	public static int PlayersAlive;
 	//e.g. playerType[1] = PlayerType.APPLE;
 	//e.g. playerType[2] would return a PlayerType value
 
