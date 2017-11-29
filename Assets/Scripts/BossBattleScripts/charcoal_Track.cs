53471d26f0735a93b7be328d73cf0ae8d5701bca
Gerardo Gonzalez <32018140+ggonzal2@users.noreply.github.com>
2017-11-29 15:40:19 -0600

    coal_Track
    coal track is done

--- Assets/Scripts/BossBattleScripts/charcoal_Track.cs
+++ Assets/Scripts/BossBattleScripts/charcoal_Track.cs
@@ -5,15 +5,49 @@ using UnityEngine;
 public class charcoal_Track : MonoBehaviour {
 
     public float speed;
-    private vector3
+    public Transform lT;
+    public Transform lB;
+    public Transform rT;
+    public Transform rB;
+    private Transform target;
 
-	// Use this for initialization
-	void Start () {
-		
+
+
+    // Use this for initialization
+    void Start () {
+        //  Vector3 ogPos = transform.position;
+        transform.position = lT.position;
+        target = lT;
 	}
 	
 	// Update is called once per frame
 	void Update () {
-		
+        float step = Time.deltaTime * speed;
+
+        transform.position = Vector3.MoveTowards(transform.position, target.position, step);
+
+
+        if (transform.position == lT.position ) {
+            target = lB;
+        }
+        
+        if (transform.position == rT.position)
+        {
+            target = lT;
+        }
+
+
+       if (transform.position == lB.position)
+        {
+            
+            target = rB;
+        }
+
+       if (transform.position == rB.position)
+        {
+            
+            target = rT;
+        }
+
 	}
 }
